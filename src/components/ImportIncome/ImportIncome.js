04ebec70f8c1c825d7972d7c6178018c33e97240
hendrysiak <lukasz.hendrysiak@gmail.com>
2020-02-05 13:56:31 +0100

    Deleted pharagraph with link

--- src/components/ImportIncome/ImportIncome.js
+++ src/components/ImportIncome/ImportIncome.js
@@ -17,11 +17,7 @@ class ImportIncome extends Component {
     }
     return (
       <section className="Section">
-        <p>
-          Chwilowo skopiuj to:
-          https://gist.githubusercontent.com/hendrysiak/9b7f2fa73d9384e3c412fcab3f8cff6c/raw/8eee22684e05fe23d515f3aa3676894ff2583191/xml-convert
-        </p>
-        <form className={classes.Form} onSubmit={event => this.setUrl(event)}>
+            <form className={classes.Form} onSubmit={event => this.setUrl(event)}>
           <h2>Wstaw URL z importem XML</h2>
           <input type="text" className={classes.Input} />
           <button type="submit" className={classes.Button}>
