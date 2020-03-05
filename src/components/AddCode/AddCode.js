127b13327b3f47607f77c46c46f2c0a79a87fb2d
hendrysiak <lukasz.hendrysiak@gmail.com>
2020-03-05 16:28:57 +0100

    Ended AddCode.js component with logic

--- src/components/AddCode/AddCode.js
+++ src/components/AddCode/AddCode.js
@@ -1,42 +1,104 @@
 import React, { useState, useEffect } from "react";
 import axios from "../../axios-income";
+import ListContainer from "../ListContainer/ListContainer";
 
 const AddCode = () => {
-  const [codes, getCodes] = useState({});
+  const [codes, setCodes] = useState([]);
 
-  const getCodesFromServer = async () => {
-    const codes = await axios.get("/codes.json");
-    await getCodes(codes.data);
+  const setCodesFromServer = async () => {
+    try {
+      const codes = await axios.get("/codes.json");
+      let codesToSave = [];
+      for (let team in codes.data) {
+        codesToSave.push({ team, codes: codes.data[team] });
+      }
+      await setCodes(codesToSave);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
-    getCodesFromServer();
+    setCodesFromServer();
   }, []);
+
+  let listOfCode;
+  if (codes.length) {
+    listOfCode = codes.map((element, index) => (
+      <ListContainer
+        key={index}
+        title={element.team !== "general" ? element.team : "Kody ogólne"}
+      >
+        {element.codes.map((code, i) => (
+          <li key={i}>{code}</li>
+        ))}
+      </ListContainer>
+    ));
+  }
+
+  const saveCode = event => {
+    event.preventDefault();
+    const id = event.target.dataset.id;
+    const dataForm = document.getElementById(`${id}`);
+    const info = new FormData(dataForm);
+    const newInfo = [...info];
+    const codesToEdit = [...codes];
+    if (newInfo.length > 1) {
+      let team = newInfo[1][1];
+      let code = newInfo[0][1];
+      let index = codesToEdit.findIndex(item => item.team === `${team}`);
+      index > -1
+        ? codesToEdit[index].codes.push(`${code}`)
+        : codesToEdit.push({ team, codes: [`${code}`] });
+      setCodes(codesToEdit);
+    } else {
+      let code = newInfo[0][1];
+      let index = codesToEdit.findIndex(item => item.team === "general");
+      codesToEdit[index].codes.push(`${code}`);
+      setCodes(codesToEdit);
+    }
+  };
+
+  const sendCode = async () => {
+    try {
+      const newCodes = {};
+      codes.forEach(team => (newCodes[team.team] = team.codes));
+      console.log(newCodes);
+      await axios.patch("/codes.json", newCodes);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div>
-      <form action="submit">
+      <form id="general">
         Dodaj kod ogólny
         <label htmlFor="general">
-          <input type="text" id="general" placeholder="Wpisz kod ogólny" />
+          <input type="text" name="general" placeholder="Wpisz kod ogólny" />
         </label>
-        <button>Dodaj</button>
+        <button data-id="general" onClick={event => saveCode(event)}>
+          Dodaj
+        </button>
       </form>
-      <form action="submit">
+      <form id="teams">
         Dodaj kod do drużyny
         <label htmlFor="teams">
-          <input type="text" id="teams" placeholder="Wpisz kod ogólny" />
+          <input type="text" name="teams-code" placeholder="Wpisz kod ogólny" />
           <input
             type="text"
-            id="teams"
+            name="teams-id"
             placeholder="Wpisz drużynę, dla której przypisano kod"
           />
         </label>
-        <button>Dodaj</button>
+        <button data-id="teams" onClick={event => saveCode(event)}>
+          Dodaj
+        </button>
       </form>
       <section>
         <h2>Przypisane kody</h2>
-        {`${codes}`}
+        {listOfCode}
+        <button onClick={sendCode}>Zapisz kody do bazy</button>
       </section>
     </div>
   );
