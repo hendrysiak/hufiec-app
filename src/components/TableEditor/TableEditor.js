2d1bbf550403f12e1906534859e5635a2b6387e4
Lukasz Hendrysiak <l.hendrysiak@optimumpareto.com>
2020-11-08 14:23:37 +0100

    Some tips to produce TableEditor

--- src/components/TableEditor/TableEditor.js
+++ src/components/TableEditor/TableEditor.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+/* TODO Use MUI Table and TextInput to implement outcome editor 
+1. Method to render data with name, surname, event, invoice number, value, founding system and category
+2. Methods to edit
+3. Method to filter diplaying info.
+4. Use this component in application
+*/
+
 const TableEditor = () => {
   return (
     <div>
