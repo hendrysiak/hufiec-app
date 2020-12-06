b12c36de63c67a7bcbef8e1b0bcf70e8330ca2a6
Lukasz Hendrysiak <l.hendrysiak@optimumpareto.com>
2020-12-06 22:04:46 +0100

    Fixed file structure

--- src/components/TableEditor/AddPositionModal.js
+++ src/components/TableEditor/AddPositionModal.js
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from 'react'
-
+import { TextField, MenuItem } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import Modal from '@material-ui/core/Modal';
-
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
+
+
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
+import React, { useEffect, useState } from 'react';
 
-import { TextField, MenuItem } from '@material-ui/core';
 
 const AddPositionModal = (props) => {
   const [cash, setCash] = useState(0);  
@@ -36,7 +36,7 @@ const AddPositionModal = (props) => {
       financeMethod
     };
 
-    props.addNewPosition(props.info, data)
+    props.addNewPosition(props.info, data);
     setCash('');  
     setBilingNr(0);
     setTeam('');
@@ -46,157 +46,157 @@ const AddPositionModal = (props) => {
     setFoundingSource('');
     setOutcomeCategory('');
     setFinanceMethod('');
-  }
+  };
 
   const modalBody = <Modal 
     open={props.modalVisible}  
     onClose={() => props.setModalVisible(false)}
   >
     <Paper className="modal__paper">
-    <div className="modal">
-    <Table size="medium">
-      <TableHead>
-        <TableRow>
-          <TableCell>Kwota</TableCell>
-          {props.info !== 'income' && <TableCell>Nr faktury</TableCell>}
-          <TableCell>Drużyna</TableCell>
-          {props.info !== 'outcome' && <TableCell>Imię</TableCell>}
-          {props.info !== 'outcome' && <TableCell>Nazwisko</TableCell>}
-          <TableCell>Wydarzenie</TableCell>
-          {props.info !== 'income' && <TableCell>Źródło finansowania</TableCell>}
-          {props.info !== 'income' && <TableCell>Kategoria kosztów</TableCell>}
-          {props.info !== 'income' && <TableCell>Źródło wpływu</TableCell>}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-      <TableRow>
-          <TableCell>
-          <TextField
-            size='medium'
-            value={cash}
-            margin='dense'
-            onChange={(e) => setCash(e.target.value)}
-          />
-          </TableCell>
-        {props.info !== 'income' && <TableCell>
-      <TextField
-          size='medium'
-          value={bilingNr}
-          margin='dense'
-          onChange={(e) => setBilingNr(e.target.value)}
-        />
-        </TableCell>}
-      <TableCell data-info='team'>
-      <TextField
-          size='medium'
-          value={team}
-          margin='dense'
-          onChange={(e) => setTeam(e.target.value)}
-          select={true}
-          SelectProps={{
-            MenuProps: { disableScrollLock: true }
-          }}
-        >
-          {props.additionalData.teams.map((item) => (
-        <MenuItem key={item} value={item}>{item}</MenuItem>
-      ))}
-      </TextField>
-        </TableCell>
+      <div className="modal">
+        <Table size="medium">
+          <TableHead>
+            <TableRow>
+              <TableCell>Kwota</TableCell>
+              {props.info !== 'income' && <TableCell>Nr faktury</TableCell>}
+              <TableCell>Drużyna</TableCell>
+              {props.info !== 'outcome' && <TableCell>Imię</TableCell>}
+              {props.info !== 'outcome' && <TableCell>Nazwisko</TableCell>}
+              <TableCell>Wydarzenie</TableCell>
+              {props.info !== 'income' && <TableCell>Źródło finansowania</TableCell>}
+              {props.info !== 'income' && <TableCell>Kategoria kosztów</TableCell>}
+              {props.info !== 'income' && <TableCell>Źródło wpływu</TableCell>}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <TextField
+                  size="medium"
+                  value={cash}
+                  margin="dense"
+                  onChange={(e) => setCash(e.target.value)}
+                />
+              </TableCell>
+              {props.info !== 'income' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={bilingNr}
+                  margin="dense"
+                  onChange={(e) => setBilingNr(e.target.value)}
+                />
+              </TableCell>}
+              <TableCell data-info="team">
+                <TextField
+                  size="medium"
+                  value={team}
+                  margin="dense"
+                  onChange={(e) => setTeam(e.target.value)}
+                  select={true}
+                  SelectProps={{
+                    MenuProps: { disableScrollLock: true }
+                  }}
+                >
+                  {props.additionalData.teams.map((item) => (
+                    <MenuItem key={item} value={item}>{item}</MenuItem>
+                  ))}
+                </TextField>
+              </TableCell>
 
-        {props.info !== 'outcome' && <TableCell>
-      <TextField
-          size='medium'
-          value={name}
-          margin='dense'
-          onChange={(e) => setName(e.target.value)}
-        />
-        </TableCell>}
-      {props.info !== 'outcome' &&  <TableCell>
-      <TextField
-          size='medium'
-          value={surname}
-          margin='dense'
-          onChange={(e) => setSurname(e.target.value)}
-        />
-        </TableCell>}
-      <TableCell>
-      <TextField
-          size='medium'
-          value={event}
-          margin='dense'
-          onChange={(e) => setEvent(e.target.value)}
-          select={true}
-          SelectProps={{
-            MenuProps: { disableScrollLock: true }
-          }}
-        >
-          {props.additionalData.codes.map((item) => (
-        <MenuItem key={item} value={item}>{item}</MenuItem>
-      ))}
-      </TextField>
-        </TableCell>
+              {props.info !== 'outcome' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={name}
+                  margin="dense"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </TableCell>}
+              {props.info !== 'outcome' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={surname}
+                  margin="dense"
+                  onChange={(e) => setSurname(e.target.value)}
+                />
+              </TableCell>}
+              <TableCell>
+                <TextField
+                  size="medium"
+                  value={event}
+                  margin="dense"
+                  onChange={(e) => setEvent(e.target.value)}
+                  select={true}
+                  SelectProps={{
+                    MenuProps: { disableScrollLock: true }
+                  }}
+                >
+                  {props.additionalData.codes.map((item) => (
+                    <MenuItem key={item} value={item}>{item}</MenuItem>
+                  ))}
+                </TextField>
+              </TableCell>
 
-        {props.info !== 'income' && <TableCell>
-      <TextField
-          size='medium'
-          value={foundingSource}
-          margin='dense'
-          onChange={(e) => setFoundingSource(e.target.value)}
-          select={true}
-          SelectProps={{
-            MenuProps: { disableScrollLock: true }
-          }}
-        >
-          {props.additionalData.foundingSources.map((item) => (
-        <MenuItem key={item} value={item}>{item}</MenuItem>
-      ))}
-      </TextField>
-        </TableCell>}
-        {props.info !== 'income' && <TableCell>
-      <TextField
-          size='medium'
-          value={outcomeCategory}
-          margin='dense'
-          onChange={(e) => setOutcomeCategory(e.target.value)}
-          select={true}
-          SelectProps={{
-            MenuProps: { disableScrollLock: true }
-          }}
-        >
-          {props.additionalData.outcomeCategory.map((item) => (
-        <MenuItem key={item} value={item}>{item}</MenuItem>
-      ))}
-      </TextField>
-        </TableCell>}
-        {props.info !== 'income' && <TableCell>
-      <TextField
-          size='medium'
-          value={financeMethod}
-          margin='dense'
-          onChange={(e) => setFinanceMethod(e.target.value)}
-          select={true}
-          SelectProps={{
-            MenuProps: { disableScrollLock: true }
-          }}
-        >
-          {['transfer', 'cash'].map((item) => (
-        <MenuItem key={item} value={item}>{item}</MenuItem>
-      ))}
-      </TextField>
-        </TableCell>}
-    </TableRow>
-      </TableBody>
-    </Table>
-    <Button variant="contained" color="primary" onClick={() => {
-      props.setModalVisible(false);
-      if (props.add) props.setAddingNewPosition(false)
-      addNewPosition();
-      }}>Dodaj nową pozycję</Button>
-    </div>
+              {props.info !== 'income' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={foundingSource}
+                  margin="dense"
+                  onChange={(e) => setFoundingSource(e.target.value)}
+                  select={true}
+                  SelectProps={{
+                    MenuProps: { disableScrollLock: true }
+                  }}
+                >
+                  {props.additionalData.foundingSources.map((item) => (
+                    <MenuItem key={item} value={item}>{item}</MenuItem>
+                  ))}
+                </TextField>
+              </TableCell>}
+              {props.info !== 'income' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={outcomeCategory}
+                  margin="dense"
+                  onChange={(e) => setOutcomeCategory(e.target.value)}
+                  select={true}
+                  SelectProps={{
+                    MenuProps: { disableScrollLock: true }
+                  }}
+                >
+                  {props.additionalData.outcomeCategory.map((item) => (
+                    <MenuItem key={item} value={item}>{item}</MenuItem>
+                  ))}
+                </TextField>
+              </TableCell>}
+              {props.info !== 'income' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={financeMethod}
+                  margin="dense"
+                  onChange={(e) => setFinanceMethod(e.target.value)}
+                  select={true}
+                  SelectProps={{
+                    MenuProps: { disableScrollLock: true }
+                  }}
+                >
+                  {['transfer', 'cash'].map((item) => (
+                    <MenuItem key={item} value={item}>{item}</MenuItem>
+                  ))}
+                </TextField>
+              </TableCell>}
+            </TableRow>
+          </TableBody>
+        </Table>
+        <Button variant="contained" color="primary" onClick={() => {
+          props.setModalVisible(false);
+          if (props.add) props.setAddingNewPosition(false);
+          addNewPosition();
+        }}>Dodaj nową pozycję</Button>
+      </div>
     </Paper>
-  </Modal>
+  </Modal>;
 
-  return (<>{modalBody}</>)
-}
+  return (<>{modalBody}</>);
+};
 
 export default AddPositionModal;
--- src/components/TableEditor/EditorModal.js
+++ src/components/TableEditor/EditorModal.js
@@ -1,19 +1,19 @@
-import React, {useEffect, useState} from 'react'
-
+import { TextField, MenuItem } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import Modal from '@material-ui/core/Modal';
 
+
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
 import EditIcon from '@material-ui/icons/Edit';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
+import React, { useEffect, useState } from 'react';
 
-import { TextField, MenuItem } from '@material-ui/core';
 
 const EditorModal = (props) => {
 
@@ -23,165 +23,165 @@ const EditorModal = (props) => {
     onClose={() => props.setModalVisible(false)}
   >
     <Paper className="modal__paper">
-    <div className="modal">
-    <Table size="medium">
-      <TableHead>
-        <TableRow>
-          <TableCell>LP</TableCell>
-          <TableCell>Tytuł</TableCell>
-          <TableCell>Kwota</TableCell>
-          {props.info !== 'income' && <TableCell>Nr faktury</TableCell>}
-          <TableCell>Drużyna</TableCell>
-          {props.info !== 'outcome' && <TableCell>Imię</TableCell>}
-          {props.info !== 'outcome' && <TableCell>Nazwisko</TableCell>}
-          <TableCell>Wydarzenie</TableCell>
-          {props.info !== 'income' && <TableCell>Źródło finansowania</TableCell>}
-          {props.info !== 'income' && <TableCell>Kategoria kosztów</TableCell>}
-          {props.info !== 'income' && <TableCell>Źródło wpływu</TableCell>}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-      <TableRow>
-      <TableCell>{props.index + 1}</TableCell>
-      {!props.add 
-        ? (<><TableCell>{props.position.title}</TableCell>
-          <TableCell>{props.position.cash}</TableCell></>)
-        : (<><TableCell>
-          <TextField
-            size='medium'
-            value={props.position.title}
-            margin='dense'
-            onChange={(e) => props.onChange(props.index, 'title', e.target.value)}
-          />
-          </TableCell>
-          <TableCell>
-          <TextField
-            size='medium'
-            value={props.position.cash}
-            margin='dense'
-            onChange={(e) => props.onChange(props.index, 'cash', e.target.value)}
-          />
-          </TableCell>
-          </>)
-      }
-        {props.info !== 'income' && <TableCell>
-      <TextField
-          size='medium'
-          value={props.position.bilingNr}
-          margin='dense'
-          onChange={(e) => props.onChange(props.index, 'bilingNr', e.target.value)}
-        />
-        </TableCell>}
-      <TableCell data-info='team'>
-      <TextField
-          size='medium'
-          value={props.position.team}
-          margin='dense'
-          onChange={(e) => props.onChange(props.index, 'team', e.target.value)}
-          select={true}
-          SelectProps={{
-            MenuProps: { disableScrollLock: true }
-          }}
-        >
-          {props.additionalData.teams.map((item) => (
-        <MenuItem key={item} value={item}>{item}</MenuItem>
-      ))}
-      </TextField>
-        </TableCell>
+      <div className="modal">
+        <Table size="medium">
+          <TableHead>
+            <TableRow>
+              <TableCell>LP</TableCell>
+              <TableCell>Tytuł</TableCell>
+              <TableCell>Kwota</TableCell>
+              {props.info !== 'income' && <TableCell>Nr faktury</TableCell>}
+              <TableCell>Drużyna</TableCell>
+              {props.info !== 'outcome' && <TableCell>Imię</TableCell>}
+              {props.info !== 'outcome' && <TableCell>Nazwisko</TableCell>}
+              <TableCell>Wydarzenie</TableCell>
+              {props.info !== 'income' && <TableCell>Źródło finansowania</TableCell>}
+              {props.info !== 'income' && <TableCell>Kategoria kosztów</TableCell>}
+              {props.info !== 'income' && <TableCell>Źródło wpływu</TableCell>}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>{props.index + 1}</TableCell>
+              {!props.add 
+                ? (<><TableCell>{props.position.title}</TableCell>
+                  <TableCell>{props.position.cash}</TableCell></>)
+                : (<><TableCell>
+                  <TextField
+                    size="medium"
+                    value={props.position.title}
+                    margin="dense"
+                    onChange={(e) => props.onChange(props.index, 'title', e.target.value)}
+                  />
+                </TableCell>
+                <TableCell>
+                  <TextField
+                    size="medium"
+                    value={props.position.cash}
+                    margin="dense"
+                    onChange={(e) => props.onChange(props.index, 'cash', e.target.value)}
+                  />
+                </TableCell>
+                </>)
+              }
+              {props.info !== 'income' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={props.position.bilingNr}
+                  margin="dense"
+                  onChange={(e) => props.onChange(props.index, 'bilingNr', e.target.value)}
+                />
+              </TableCell>}
+              <TableCell data-info="team">
+                <TextField
+                  size="medium"
+                  value={props.position.team}
+                  margin="dense"
+                  onChange={(e) => props.onChange(props.index, 'team', e.target.value)}
+                  select={true}
+                  SelectProps={{
+                    MenuProps: { disableScrollLock: true }
+                  }}
+                >
+                  {props.additionalData.teams.map((item) => (
+                    <MenuItem key={item} value={item}>{item}</MenuItem>
+                  ))}
+                </TextField>
+              </TableCell>
 
-        {props.info !== 'outcome' && <TableCell>
-      <TextField
-          size='medium'
-          value={props.position.name}
-          margin='dense'
-          onChange={(e) => props.onChange(props.index, 'name', e.target.value)}
-        />
-        </TableCell>}
-      {props.info !== 'outcome' &&  <TableCell>
-      <TextField
-          size='medium'
-          value={props.position.surname}
-          margin='dense'
-          onChange={(e) => props.onChange(props.index, 'surname', e.target.value)}
-        />
-        </TableCell>}
-      <TableCell>
-      <TextField
-          size='medium'
-          value={props.position.event}
-          margin='dense'
-          onChange={(e) => props.onChange(props.index, 'event', e.target.value)}
-          select={true}
-          SelectProps={{
-            MenuProps: { disableScrollLock: true }
-          }}
-        >
-          {props.additionalData.codes.map((item) => (
-        <MenuItem key={item} value={item}>{item}</MenuItem>
-      ))}
-      </TextField>
-        </TableCell>
+              {props.info !== 'outcome' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={props.position.name}
+                  margin="dense"
+                  onChange={(e) => props.onChange(props.index, 'name', e.target.value)}
+                />
+              </TableCell>}
+              {props.info !== 'outcome' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={props.position.surname}
+                  margin="dense"
+                  onChange={(e) => props.onChange(props.index, 'surname', e.target.value)}
+                />
+              </TableCell>}
+              <TableCell>
+                <TextField
+                  size="medium"
+                  value={props.position.event}
+                  margin="dense"
+                  onChange={(e) => props.onChange(props.index, 'event', e.target.value)}
+                  select={true}
+                  SelectProps={{
+                    MenuProps: { disableScrollLock: true }
+                  }}
+                >
+                  {props.additionalData.codes.map((item) => (
+                    <MenuItem key={item} value={item}>{item}</MenuItem>
+                  ))}
+                </TextField>
+              </TableCell>
 
-        {props.info !== 'income' && <TableCell>
-      <TextField
-          size='medium'
-          value={props.position.foundingSource}
-          margin='dense'
-          onChange={(e) => props.onChange(props.index, 'foundingSource', e.target.value)}
-          select={true}
-          SelectProps={{
-            MenuProps: { disableScrollLock: true }
-          }}
-        >
-          {props.additionalData.foundingSources.map((item) => (
-        <MenuItem key={item} value={item}>{item}</MenuItem>
-      ))}
-      </TextField>
-        </TableCell>}
-        {props.info !== 'income' && <TableCell>
-      <TextField
-          size='medium'
-          value={props.position.outcomeCategory}
-          margin='dense'
-          onChange={(e) => props.onChange(props.index, 'outcomeCategory', e.target.value)}
-          select={true}
-          SelectProps={{
-            MenuProps: { disableScrollLock: true }
-          }}
-        >
-          {props.additionalData.outcomeCategory.map((item) => (
-        <MenuItem key={item} value={item}>{item}</MenuItem>
-      ))}
-      </TextField>
-        </TableCell>}
-        {props.info !== 'income' && <TableCell>
-      <TextField
-          size='medium'
-          value={props.position.financeMethod}
-          margin='dense'
-          onChange={(e) => props.onChange(props.index, 'financeMethod', e.target.value)}
-          select={true}
-          SelectProps={{
-            MenuProps: { disableScrollLock: true }
-          }}
-        >
-          {['transfer', 'cash'].map((item) => (
-        <MenuItem key={item} value={item}>{item}</MenuItem>
-      ))}
-      </TextField>
-        </TableCell>}
-    </TableRow>
-      </TableBody>
-    </Table>
-    <Button variant="contained" color="primary" onClick={() => {
-      props.setModalVisible(false);
-      }}>Zakończ edycję</Button>
-    </div>
+              {props.info !== 'income' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={props.position.foundingSource}
+                  margin="dense"
+                  onChange={(e) => props.onChange(props.index, 'foundingSource', e.target.value)}
+                  select={true}
+                  SelectProps={{
+                    MenuProps: { disableScrollLock: true }
+                  }}
+                >
+                  {props.additionalData.foundingSources.map((item) => (
+                    <MenuItem key={item} value={item}>{item}</MenuItem>
+                  ))}
+                </TextField>
+              </TableCell>}
+              {props.info !== 'income' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={props.position.outcomeCategory}
+                  margin="dense"
+                  onChange={(e) => props.onChange(props.index, 'outcomeCategory', e.target.value)}
+                  select={true}
+                  SelectProps={{
+                    MenuProps: { disableScrollLock: true }
+                  }}
+                >
+                  {props.additionalData.outcomeCategory.map((item) => (
+                    <MenuItem key={item} value={item}>{item}</MenuItem>
+                  ))}
+                </TextField>
+              </TableCell>}
+              {props.info !== 'income' && <TableCell>
+                <TextField
+                  size="medium"
+                  value={props.position.financeMethod}
+                  margin="dense"
+                  onChange={(e) => props.onChange(props.index, 'financeMethod', e.target.value)}
+                  select={true}
+                  SelectProps={{
+                    MenuProps: { disableScrollLock: true }
+                  }}
+                >
+                  {['transfer', 'cash'].map((item) => (
+                    <MenuItem key={item} value={item}>{item}</MenuItem>
+                  ))}
+                </TextField>
+              </TableCell>}
+            </TableRow>
+          </TableBody>
+        </Table>
+        <Button variant="contained" color="primary" onClick={() => {
+          props.setModalVisible(false);
+        }}>Zakończ edycję</Button>
+      </div>
     </Paper>
           
-  </Modal>) : <></>
+  </Modal>) : <></>;
 
-  return (<>{modalBody}</>)
-}
+  return (<>{modalBody}</>);
+};
 
 export default EditorModal;
