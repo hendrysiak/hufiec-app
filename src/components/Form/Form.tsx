09b1b892d0a0bf60dc20e1354f140c651ac3d077
Tubka <krzysiek199401@poczta.fm>
2021-01-03 22:12:38 +0100

    modified component form to MUI

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,3 +1,4 @@
+import { Button, TextField } from '@material-ui/core';
 import Modal from '@material-ui/core/Modal';
 import MailIcon from '@material-ui/icons/Mail';
 import React, { useState } from 'react';
@@ -27,9 +28,33 @@ const Form = ({title }: {title: string }): JSX.Element => {
         <>
           <form className={classes.positionModal}>
             <h1>{title}</h1>
-            <label className={classes.label} htmlFor="input"><input className={classes.input} id="input" type="text" placeholder="WPISZ TEMAT"/></label>
-            <textarea className={classes.textarea} name="report" cols={30} rows={10} placeholder="WPISZ TREŚĆ ZGŁOSZENIA"></textarea>
-            <button className={classes.button}>Wyślij</button>
+            {/* <label className={classes.label} htmlFor="input"><input className={classes.input} id="input" type="text" placeholder="WPISZ TEMAT"/></label> */}
+            {/* <textarea className={classes.textarea} name="report" cols={30} rows={10} placeholder="WPISZ TREŚĆ ZGŁOSZENIA"></textarea> */}
+            {/* <TextField
+              id="standard-multiline-flexible"
+              label="Multiline"
+              multiline
+              rowsMax={4}
+            />
+            <TextField
+              id="standard-textarea"
+              label="Multiline Placeholder"
+              placeholder="Placeholder"
+              multiline
+            /> */}
+            <TextField style={{width: '80%'}} id="standard-basic" label="WPISZ TEMAT" required />
+            <TextField
+              style={{width: '80%'}}
+              fullWidth
+              id="standard-multiline-static"
+              label="WPISZ WIADOMOŚĆ"
+              multiline
+              rows={10}
+              required
+              // defaultValue="Default Value"
+            />
+            <Button style={{margin: '10px 0 10px 0'}} variant="contained" color="primary">WYŚLIJ</Button>
+            {/* <button className={classes.button}>Wyślij</button> */}
           </form>
         </>
       </Modal>
