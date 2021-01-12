f268c2d36cd49f90b5262020d5fe451522ef5049
Tubka <krzysiek199401@poczta.fm>
2021-01-12 21:25:00 +0100

    created function to check correctness of the format price, regexp

--- src/pages/AddPercent/AddPercent.tsx
+++ src/pages/AddPercent/AddPercent.tsx
@@ -1,8 +1,40 @@
-import React, { FC } from 'react'
+import { Button } from '@material-ui/core';
+import Input from '@material-ui/core/Input';
+import React, { FC, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import Navigation from 'shared/Navigation/Navigation';
+import { RootState } from 'store/models/rootstate.model';
+
+import classes from './AddPercent.module.css';
 
 const AddPercent: FC = () => {
+  const kwota = 1200
+  const registry = useSelector((state: RootState) => state.income.registry);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSendNewValue = (team: string, value: string) => {
+    if (/^[0-9]+(\.\d{2})$/.test(value)) {
+      alert('poszedł zapis'); // function to send data
+      return;
+    }
+    alert('Zły format, 2miejsca po przecinku oddziel kropką.');
+  };
+
   return (
-    <div>test</div>
+    <>
+      <Navigation />
+      {registry && Object.entries(registry).map((el, index) => (
+        <div className={classes.oneRow} key={index}>
+          <p>DRUŻYNA <span className={classes.bold}>{el[0]}</span> | OBECNIE: {kwota} zł</p>
+          <label htmlFor=""> Nowa kwota: <Input type="text" placeholder="kwota" onChange={handleInput}/></label>
+          <Button onClick={() => handleSendNewValue(el[0], inputValue)} variant="contained" color="primary">ZAPISZ</Button>
+        </div>)) }
+    </>
   );
 };
 
