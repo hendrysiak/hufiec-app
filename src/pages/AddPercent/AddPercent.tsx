6cfa8cc6763e3e8e3d414839e653f5e76b8ac0d8
Tubka <krzysiek199401@poczta.fm>
2021-01-12 18:43:43 +0100

    add new item in navigation

---
+++ src/pages/AddPercent/AddPercent.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+const AddPercent: FC = () => {
+  return (
+    <div>test</div>
+  );
+};
+
+export default AddPercent;
--- src/shared/Navigation/Navigation.tsx
+++ src/shared/Navigation/Navigation.tsx
@@ -32,6 +32,7 @@ const Navigation = (): JSX.Element => {
     { link: '/add-billing', title: 'DODAJ ROZLICZENIE', icon: <PlaylistAddCheckIcon fontSize="small" /> },
     { link: '/editor', title: 'EDYTUJ PRZYCHODY/KOSZTY', icon: <EditIcon fontSize="small" /> },
     { link: '/edit-team', title: 'EDYTUJ DRUŻYNĘ', icon: <EditIcon fontSize="small" /> },
+    { link: '/addpercent', title: 'DODAJ 1%', icon: <EditIcon fontSize="small" /> },
   ];
 
   return (
