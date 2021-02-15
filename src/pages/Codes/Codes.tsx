b76f450b6087b20b20aaafc1d21359d9a0d67b9d
Lukasz Hendrysiak <l.hendrysiak@optimumpareto.com>
2021-02-15 07:49:26 +0100

    Refactor Codes.tsx in progress

--- src/pages/Codes/Codes.tsx
+++ src/pages/Codes/Codes.tsx
@@ -1,13 +1,27 @@
 import { TextField, MenuItem } from '@material-ui/core';
+import { DataGrid } from '@material-ui/data-grid';
+
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-import ListContainer from 'shared/ListContainer/ListContainer';
+import { IncomeDb } from 'models/income.models';
+
+// import ListContainer from 'shared/ListContainer/ListContainer';
 import ListEl from 'shared/ListEl/ListEl';
 import Navigation from 'shared/Navigation/Navigation';
 import { RootState } from 'store/models/rootstate.model';
 
 import classes from './Codes.module.css';
+
+interface Row {
+  id: string;
+  lp: number;
+  name: string | null;
+  surname: string | null;
+  team: string | null;
+  cash: number;
+  dateOfBook: string;
+}
 
 const Codes = (): JSX.Element => {
   const dbIncomes = useSelector((state: RootState) => state.income.dbIncomes);
@@ -15,6 +29,18 @@ const Codes = (): JSX.Element => {
 
   const [ usedCodes, setUsedCodes ] = useState<string[]>([]);
   const [ currentCode, setCurrentCode ] = useState<string>('');
+
+  const [rows, setRows] = useState<Row[]>();
+  
+  const columns = [
+    { field: 'lp', headerName: 'LP', width: 60, },
+    { field: 'name', headerName: 'Imię', width: 150 },
+    { field: 'surname', headerName: 'Nazwisko', width: 150 },
+    { field: 'team', headerName: 'Drużyna', width: 150 },
+    { field: 'cash', headerName: 'Kwota', width: 150 },
+    { field: 'dateOfBook', headerName: 'Data wpłaty', width: 120 },
+    { field: 'title', headerName: 'Tytuł', width: 340 }
+  ];
 
   useEffect(() => {
     if (codes) {
@@ -24,49 +50,73 @@ const Codes = (): JSX.Element => {
     }
   }, [codes]);
 
+  useEffect(() => {
+    const rows = currentCode === 'Błedy niewyjaśnione' 
+      ? dbIncomes && dbIncomes.filter(i => !i.event || !i.name || !i.surname || !i.team || !i.year ).map((income, index) => {
+        return {
+          ...income,
+          lp: index + 1,
+          dateOfBook: new Date(income.dateOfBook).toLocaleDateString()
+        };
+      })
+      : dbIncomes && dbIncomes.filter(i => i.event === currentCode).map((income, index) => {
+        return {
+          ...income,
+          lp: index + 1,
+          dateOfBook: new Date(income.dateOfBook).toLocaleDateString()
+        };
+      });
 
-  const children = currentCode === 'Brak kodu' 
-    ? dbIncomes && dbIncomes.filter(i => !i.event).map((income, index) => {
-      return <ListEl error={false} key={index} title={income.title} cash={income.cash} />;
-    })
-    : dbIncomes && dbIncomes.filter(i => i.event === currentCode).map((income, index) => {
-      return <ListEl error={false} key={index} title={income.title} cash={income.cash} />;
-    });
+    setRows(rows);
+  },[currentCode]);
+
+
+  // const children = currentCode === 'Błedy niewyjaśnione' 
+  //   ? dbIncomes && dbIncomes.filter(i => !i.event).map((income, index) => {
+  //     return <ListEl error={false} key={index} title={income.title} cash={income.cash} />;
+  //   })
+  //   : dbIncomes && dbIncomes.filter(i => i.event === currentCode).map((income, index) => {
+  //     return <ListEl error={false} key={index} title={income.title} cash={income.cash} />;
+  //   });
 
   return (
     <>
       <Navigation />
-      <section className="Section">
-        <header>
+      <header>
 
-          <TextField 
-            style={{ width: '80%', marginTop: '16px' }}
-            value={currentCode}
-            onChange={(e) => setCurrentCode(e.target.value)}
-            placeholder="Wybierz kod z listy"
-            select={true}
-            size="small"
-            variant="outlined"
-            margin="normal"
-            SelectProps={{
-              MenuProps: { disableScrollLock: true }
-            }}
-          >
-            {usedCodes && [...usedCodes, 'Brak kodu'].map((item) => (
-              <MenuItem key={item} value={item}>{item}</MenuItem>
-            ))}
-          </TextField>
-          <h2>Pokaż listę po kodzie</h2>
+        <TextField 
+          style={{ width: '80%', marginTop: '16px' }}
+          value={currentCode}
+          onChange={(e) => setCurrentCode(e.target.value)}
+          placeholder="Wybierz kod z listy"
+          select={true}
+          size="small"
+          variant="outlined"
+          margin="normal"
+          SelectProps={{
+            MenuProps: { disableScrollLock: true }
+          }}
+        >
+          {usedCodes && [...usedCodes, 'Błedy niewyjaśnione'].map((item) => (
+            <MenuItem key={item} value={item}>{item}</MenuItem>
+          ))}
+        </TextField>
+        <h2>Lista po kodzie</h2>
 
-        </header>
-        <main className={classes.Main}>
-          {children && <ListContainer
+      </header>
+      {/* <main className={classes.Main}> */}
+      {/* {children && <ListContainer
             title={currentCode}
           >
             {children}
-          </ListContainer>}
-        </main>
-      </section>
+          </ListContainer>} */}
+      {rows?.length ? (
+        <DataGrid rows={rows} columns={columns} pageSize={rows.length} autoHeight={true}/> 
+      ) : (
+        <div>Brak wpłat na kod</div>
+      )
+      }
+      {/* </main> */}
     </>
   );
 };
