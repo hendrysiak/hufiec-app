b5bf7a775611ee1c5a75f36553417b50cba97f09
lhendrysiak <lukasz.hendrysiak@noaignite.com>
2022-01-11 07:41:51 +0100

    Implemented ReAccounting letter creator

--- src/pages/Letter/Letter.tsx
+++ src/pages/Letter/Letter.tsx
@@ -1,8 +1,14 @@
+import DownloadIcon from '@mui/icons-material/Download';
+import CircularProgress from '@mui/material/CircularProgress';
+import Tooltip from '@mui/material/Tooltip';
 import { Document, Page, Font, Text, View, StyleSheet, PDFDownloadLink } from '@react-pdf/renderer';
+
 
 import React from 'react';
 
 import font from 'fonts/Museo300-Regular.ttf';
+
+import { ProposalArea, ProposalKind } from 'models/global.enum';
 
 import Footer from './containers/Footer/Footer';
 
@@ -14,6 +20,10 @@ import Main from './containers/Main/Main';
 interface LetterProps {
   recipient: string;
   author?: string;
+  area: ProposalArea;
+  kind: ProposalKind;
+  oldValues: unknown;
+  newValues: unknown;
 }
 
 Font.register({ family: 'Museo300', src: font });
@@ -44,7 +54,13 @@ const Letter = (props: LetterProps): JSX.Element => {
             <Page size="A4" orientation="portrait" style={pageStyle.page}>
               {/* <View> */}
               <Header recipient={props.recipient} />
-              <Main author={props.author} />
+              <Main 
+                author={props.author}
+                area={props.area}
+                kind={props.kind}
+                oldValues={props.oldValues}
+                newValues={props.newValues}
+              />
               <Footer />
             </Page>
           </Document>
@@ -52,7 +68,16 @@ const Letter = (props: LetterProps): JSX.Element => {
         fileName="pismo.pdf"
       
       >
-        {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
+        {({ blob, url, loading, error }) => (
+          <div 
+            style={{ 
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center'
+            }}
+          >
+            {loading ? <CircularProgress /> : <Tooltip placement="top" title="Pobierz pismo"><DownloadIcon /></Tooltip>}
+          </div>)}
       </PDFDownloadLink>
     </>
   );
