b3b7b15c558a71ea800d0f749065a61091cbb510
lhendrysiak <lukasz.hendrysiak@noaignite.com>
2022-10-27 07:43:55 +0200

    Fix eslint error

--- src/pages/ImportIncome/helpers/setReduxData.helper.ts
+++ src/pages/ImportIncome/helpers/setReduxData.helper.ts
@@ -3,6 +3,8 @@ import convert, { ElementCompact, Element } from 'xml-js';
 import { reduxSetIncome } from 'store/actions/income';
 import store from 'store/store';
 
+import { INGBilingDocument } from '../models/ing.biling.model';
+
 import { sortBilingFromING } from './bank.helper';
 
 // export const setIncomeInRedux = async (url: string): Promise<void> => {
@@ -12,8 +14,7 @@ export const setIncomeInRedux = async (file: File): Promise<void> => {
     const result = reader.result;
     
     if (typeof result === 'string') {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const convertedJson: any = convert.xml2js(result, { compact: true });
+      const convertedJson = convert.xml2js(result, { compact: true }) as Record<string, INGBilingDocument>;
       const sortedJSON = sortBilingFromING(convertedJson.Document);
       store.dispatch(reduxSetIncome(sortedJSON));
     }
