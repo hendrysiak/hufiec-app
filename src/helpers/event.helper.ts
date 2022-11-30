5b35b47713a90f2a45dbd199b473396d353eab4c
lhendrysiak <lukasz.hendrysiak@noaignite.com>
2022-11-30 07:58:41 +0100

    Extended list of available events

--- src/helpers/event.helper.ts
+++ src/helpers/event.helper.ts
@@ -4,7 +4,7 @@ export const codePattern = [
   { name: 'biwak szczepu', value: 'BS', useSuffix: true, adminEvent: false },
   { name: 'rajd', value: 'RD', useSuffix: true, adminEvent: false },
   { name: 'harcerski start', value: 'HS', useSuffix: false, adminEvent: true },
-  { name: 'CMOK SONG', value: 'CMOK', useSuffix: false, adminEvent: true },
+  { name: 'CMOK SONG', value: 'CS', useSuffix: false, adminEvent: true },
   { name: 'Odrzański Spływ Wiosenny', value: 'OSW', useSuffix: false, adminEvent: false },
   { name: 'Jesienny Spływ Wiślany', value: 'JSW', useSuffix: false, adminEvent: false },
   { name: 'obóz', value: 'OH', useSuffix: true, adminEvent: false },
@@ -17,4 +17,7 @@ export const codePattern = [
   { name: 'zlot chorągwi', value: 'ZC', useSuffix: true, adminEvent: true },
   { name: 'wyjazdowy festiwal', value: 'WF', useSuffix: true, adminEvent: true },
   { name: 'Betlejemskie Światło Pokoju', value: 'BSP', useSuffix: true, adminEvent: true },
+  { name: 'Wigilijka drużyny', value: 'WIG', useSuffix: true, adminEvent: false },
+  { name: 'Warsztaty', value: 'WAR', useSuffix: true, adminEvent: false },
+  { name: 'Bal', value: 'BAL', useSuffix: true, adminEvent: false },
 ];
