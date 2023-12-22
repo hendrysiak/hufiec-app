492a773d5b57df8dc1a17fb9caf6771997e08561
lhendrysiak <lukasz.hendrysiak@noaignite.com>
2023-12-22 14:25:14 +0100

    Fixed permission provider

--- src/providers/PermissionsProvider/PermissionsProvider.tsx
+++ src/providers/PermissionsProvider/PermissionsProvider.tsx
@@ -11,6 +11,9 @@ import { reduxSetEvidenceNumber } from 'store/actions/user';
 import store from 'store/store';
 
 import { useAuth } from '../AuthUserProvider/AuthUserProvider';
+import { Typography, Button, Avatar, Dialog, DialogTitle, List, ListItem, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
+import { blue } from '@mui/material/colors';
+import GroupsIcon from '@mui/icons-material/Groups';
 
 const downloadData = async (team: number) => {
   await getInitAccountState();
@@ -20,44 +23,113 @@ const downloadData = async (team: number) => {
   await getImportDates();
 };
 
-export function PermissionsProvider({ children } : { children: React.ReactElement }) {
+export interface SimpleDialogProps {
+  open: boolean;
+  selectedValue: string;
+  onClose: (value: string) => void;
+  teams: string[];
+}
+
+function SimpleDialog(props: SimpleDialogProps) {
+  const { onClose, selectedValue, open, teams } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+  const handleListItemClick = (value: string) => {
+    onClose(value);
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Wybierz drużynę z listy</DialogTitle>
+      <List sx={{ pt: 0 }}>
+        {teams.map((team) => (
+          <ListItem disableGutters key={team}>
+            <ListItemButton onClick={() => handleListItemClick(team)}>
+              <ListItemAvatar>
+                <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                  <GroupsIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={team} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Dialog>
+  );
+}
+
+export default function ChooseTeamDialog({
+  teams,
+  open,
+  setOpen,
+  selectedValue,
+  setSelectedValue
+}: {
+  teams: string[],
+  open: boolean,
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>,
+  selectedValue: string,
+  setSelectedValue: (team: string) => void
+}) {
+
+  const handleClose = (value: string) => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
+
+  return (
+    <div>
+      <SimpleDialog
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+        teams={teams}
+      />
+    </div>
+  );
+}
+
+export function PermissionsProvider({ children }: { children: React.ReactElement }) {
   const { authUser } = useAuth();
   const history = useHistory();
   const location = useLocation();
+  const [team, setTeam] = React.useState<number | null>(null);
+  const [open, setOpen] = React.useState(false);
 
   const user = useUserData(authUser?.uid);
+
+  const handleSelectTeam = (newTeam: string) => {
+    setTeam(Number(newTeam));
+  }
 
   // THere we want to provide check function to make sure that the user with correct role has access to correct data
 
   React.useEffect(() => {
     if (user) {
-      let team = null;
+      const currentUrl = location.pathname;
 
-      if (history.location.pathname !== '/') {
-        const url = Number(history.location.pathname.slice(1));
-        team = !Number.isNaN(url) ? url : null;
-      }
+      if (user?.team) {
+        if (currentUrl !== '/') {
+          const potentialTeam = history.location.pathname.slice(1);
+          if (user?.team.includes(potentialTeam)) {
+            return setTeam(Number(potentialTeam));
+          }
+        }
 
-      if (user?.team && !team) {
-        if (user?.team.length > 1) {
-        const choosenTeam = window.prompt(`Wybierz zespół spośród dostępnych opcji: ${user.team.map(team => team)}`, user.team[1]);
-        team = choosenTeam ? Number(choosenTeam) : 1111;
-        } else {
-          team = Number(user.team[0]);
+        if (!team) {
+          if (user?.team.length > 1) {
+            setOpen(true)
+          } else {
+            setTeam(Number(user.team[0]));
+          }
         }
       }
 
-        store.dispatch(reduxSetRoles([user.role]));
-        store.dispatch(reduxIsAuthentication(true));
-        store.dispatch(reduxSetEvidenceNumber(user.evidenceNumber ?? ''));
-        store.dispatch(reduxSetTeam(team ?? 1111));
-        downloadData(team ?? 1111);
 
-      
-      if (user?.role === 'leader') {
-        location.pathname !== `/${team}` ? history?.push(`/${team}`) : null;
-      }
-  
       if (user?.role === 'admin' && location.pathname === '/') {
         history?.push('/dashboard');
       }
@@ -68,5 +140,28 @@ export function PermissionsProvider({ children } : { children: React.ReactElemen
     }
   }, [location.pathname, user]);
 
-  return <>{children}</>;
+  React.useEffect(() => {
+    history?.push(`/${team}`)
+  }, [team]);
+
+  React.useEffect(() => {
+    if (user) {
+      store.dispatch(reduxSetRoles([user.role]));
+      store.dispatch(reduxIsAuthentication(true));
+      store.dispatch(reduxSetEvidenceNumber(user.evidenceNumber ?? ''));
+      store.dispatch(reduxSetTeam(team ?? 1111));
+      downloadData(team ?? 1111);
+    }
+  }, [user, team]);
+
+  return (<>
+    {children}
+    <ChooseTeamDialog
+      teams={user?.team ?? []}
+      open={open}
+      setOpen={setOpen}
+      selectedValue={`${team}` ?? '1111'}
+      setSelectedValue={handleSelectTeam}
+    />
+  </>);
 }
