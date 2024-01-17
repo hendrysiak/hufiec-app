ffb44d7cf7b1b16a3b57db8240715b2ddcb4c3c2
lhendrysiak <lukasz.hendrysiak@noaignite.com>
2024-01-17 08:49:22 +0100

    Extended AdminActions with new logic behind visibility of component

--- src/components/AdminActions/AdminActions.tsx
+++ src/components/AdminActions/AdminActions.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Grid, Modal, Paper } from "@mui/material";
 import ModalMapInitAccountToRegistry from "components/ModalMapInitAccountToRegistry/ModalMapInitAccountToRegistry";
+import ModalTipiImport from "components/ModalTipiImport/ModalTipiImport";
 import { getPlainRegistry } from "helpers/api-helpers/account.handler";
 import { deleteAllOutcomes, deleteIncomesByCode } from "helpers/editing-db.handler";
 import { useState } from "react";
@@ -9,29 +10,42 @@ import { RootState } from "store/models/rootstate.model";
 
 const AdminActions = () => {
 
-    const [open, setOpen] = useState<boolean>(false);
+  const [mapperModalOpen, setMapperModalOpen] = useState<boolean>(false);
+  const [tipiModalOpen, setTipiModalOpen] = useState<boolean>(false);
 
-    const clearAccounts = async () => {
-        if (window.confirm('Czy na pewno chcesz wyczyścić konta?')) {
-          await deleteIncomesByCode('SC');
-          await deleteAllOutcomes();
-        }
-      }
+  const clearAccounts = async () => {
+    if (window.confirm('Czy na pewno chcesz wyczyścić konta?')) {
+      await deleteIncomesByCode('SC');
+      await deleteAllOutcomes();
+    }
+  }
 
-    return (<Paper elevation={3}>
-        <Box p={4}>
-              <h2>Czyszczenie składek</h2>
-              <Box pt={3.5} pb={3.5}>
-                <Button variant="contained" color="secondary" onClick={clearAccounts}>Wyczyść konta</Button>
-              </Box>
-            <Box p={4}>
-                <h2>Dopisz numery ewidencji</h2>
-                <p>Dzięki temu dopiszesz numery ewidencji z kolekcji stanu początkowego</p>
-                <Button variant="contained" color="secondary" onClick={() => setOpen(true)}>Otwórz mapper</Button>
-            </Box>
+  return (<Paper elevation={3}>
+    <Box p={4}>
+      <h2>Czyszczenie składek</h2>
+      <Box pt={3.5} pb={3.5}>
+        <p>Dzięki temu wyczyścisz nieaktualne przelewy, dotyczące składek</p>
+      </Box>
+      <Button variant="contained" color="secondary" onClick={clearAccounts}>Wyczyść konta</Button>
+      <Box p={4}>
+        <h2>Dopisz numery ewidencji</h2>
+        <Box pt={3.5} pb={3.5}>
+          <p>Dzięki temu dopiszesz numery ewidencji z kolekcji stanu początkowego</p>
         </Box>
-        <ModalMapInitAccountToRegistry open={open} setOpen={setOpen} />
-    </Paper>)
+        <Button variant="contained" color="secondary" onClick={() => setMapperModalOpen(true)}>Otwórz mapper</Button>
+      </Box>
+      <Box p={4}>
+        <h2>Zaimportuj dane z Tipi</h2>
+        <Box pt={3.5} pb={3.5}>
+          <p>Dzięki temu zaktualizujesz bazę danych aplikacji o dane z ewidencji</p>
+
+        </Box>
+        <Button variant="contained" color="secondary" onClick={() => setTipiModalOpen(true)}>Otwórz importer</Button>
+      </Box>
+    </Box>
+    {mapperModalOpen && <ModalMapInitAccountToRegistry open={mapperModalOpen} setOpen={setMapperModalOpen} />}
+    {tipiModalOpen && <ModalTipiImport open={tipiModalOpen} setOpen={setTipiModalOpen} />}
+  </Paper>)
 };
 
 export default AdminActions;
